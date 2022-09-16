32f2ffbfe572f4279167444766c85358f6b7dc99
psydeveloper <cb.mangalsingh@gmail.com>
2022-09-17 04:57:10 +0530

    hghg

---
+++ webapicore/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapicore.Model;
+using webapicore.Model.DataContext;
+
+namespace webapicore.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly DataContext _db;
+        public HomeController(DataContext db)
+        {
+            _db = db;
+        }
+        [HttpPost]
+        public dynamic postdata(User u)
+        {
+            _db.users.Add(u);
+            _db.SaveChanges();
+            return "success";
+        }
+        [HttpGet]
+        public dynamic getdata()
+        {
+           var data =  _db.users.ToList();
+            return data;
+        }
+    }
+}
