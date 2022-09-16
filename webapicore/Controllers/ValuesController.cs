cad1ad8a95a8fa2181970a199ca5036c408b8542
psydeveloper <cb.mangalsingh@gmail.com>
2022-09-17 03:09:40 +0530

    dsf

---
+++ webapicore/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapicore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public dynamic getdata()
+        {
+            string mangal = "mangalsingh"
+            return mangal
+        }
+    }
+}
