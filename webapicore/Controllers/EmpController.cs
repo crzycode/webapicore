f28cc0f62d5c16cbdb32596e367b533ed5cce12b
psydeveloper <cb.mangalsingh@gmail.com>
2022-09-17 05:44:51 +0530

    jjh

---
+++ webapicore/Controllers/EmpController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapicore.Model;
+using webapicore.Model.DataContext;
+
+namespace webapicore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpController : ControllerBase
+    {
+        private readonly DataContext db;
+        public EmpController(DataContext _db)
+        {
+            db = _db;   
+        }
+
+        [HttpPost]
+        public dynamic postdata(Employee e)
+        {
+            db.employees.Add(e);
+            db.SaveChanges();
+            return ("succsee");
+        }
+
+    }
+}
---
+++ webapicore/Migrations/20220917001055_intiladdf.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapicore.Migrations
+{
+    public partial class intiladdf : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "employees",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmpName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Salary = table.Column<int>(type: "int", nullable: false),
+                    Disignation = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employees", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "employees");
+        }
+    }
+}
